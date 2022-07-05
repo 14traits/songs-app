fa65d37eccba4bc0ca44488c5c3a3bc0af6b5bf5
John Rogers <john.rogers@mac.com>
2022-07-05 12:28:25 -0500

    http responses page added, system is up and working

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,35 +10,35 @@ class SongsController < ApplicationController
 
   def find_song
     song = Song.find_by(id: params["id"])
-    render json: array.as_json
+    render json: song.as_json
   end
-  
+
   def create
     song = Song.new(
       id: params["id"],
       title: params["title"],
-      artist: params["artist"]
+      artist: params["artist"],
       album: params["album"],
       year: params["year"],
     )
     song.save
-    render json: array.as_json
+    render json: song.as_json
   end
-  
+
   def update
     song = Song.find_by(id: params["id"])
     song.artist = params["artist"] || song.artist
     song.title = params["title"] || song.title
     song.album = params["album"] || song.album
-    song.year = params["year"]  || song.year
-    
+    song.year = params["year"] || song.year
+
     song.save
-    render json: array.as_json
+    render json: song.as_json
   end
-  
+
   def delete
     song = Song.find_by(id: params["id"])
     song.destroy
-    render json: {message: "Song was deleted!"}
+    render json: { message: "Song was deleted!" }
   end
 end
